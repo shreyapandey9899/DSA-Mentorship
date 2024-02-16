3c650d5ceaf2de8ea30f520be6b061ce6a526667
shreyapandey9899 <143884223+shreyapandey9899@users.noreply.github.com>
2024-02-16 16:46:23 +0530

    Add files via upload

---
+++ Q2A3.java
@@ -0,0 +1,26 @@
+package Assignment3;
+
+import java.util.*;
+
+public class Q2A3 {
+    public static void main(String []args){
+        try(Scanner sc = new Scanner(System.in)){
+            int size = sc.nextInt();
+            int []arr= new int[size];
+            for(int i=0;i<size;i++){
+                arr[i]=sc.nextInt();
+            }
+            // for(int i=0;i<size;i++){
+            //     for(int j =1;j<size-1;j++){
+            //         if(arr[j]>arr[j+1]){
+            //             int temp = arr[j];
+            //             arr[j] = arr[j+1];
+            //             arr[j+1] = temp;
+            //         }
+            //     }
+            Arrays.sort(arr);
+            
+            System.out.println("maximum is: "+ arr[size-1]+"  minimum is: "+arr[0]);
+        }}
+    }
+
